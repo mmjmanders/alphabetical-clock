84a715ecffa22e6694f740ed02ba6006ae9a9a9e
Mark Manders <mmjmanders@gmail.com>
2026-04-24 13:58:46 +0200

    Adjust SecondHand path to start from center for improved alignment

--- src/components/SecondHand.vue
+++ src/components/SecondHand.vue
@@ -14,7 +14,7 @@ const rotation = computed(() => (360 / 60) * secondWords.indexOf(toWords(props.s
 
 <template>
   <g class="clock-second-hand" :transform="`rotate(${rotation})`">
-    <path stroke-width="10" :d="`M 0 0 L 0 -10 L ${length} 0 L 0 10 Z`" />
+    <path stroke-width="10" :d="`M -25 0 L 0 -10 L ${length} 0 L 0 10 Z`" />
     <circle :cx="`${length - 60}`" cy="0" r="25" />
   </g>
 </template>
