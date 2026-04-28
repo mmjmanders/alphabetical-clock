6a752eff971d47e55f29991bf45f8df958345a25
Mark Manders <mmjmanders@gmail.com>
2026-04-28 19:24:39 +0200

    Add parameterized e2e test for multilingual time display

--- e2e/time-display.spec.ts
+++ e2e/time-display.spec.ts
@@ -10,3 +10,21 @@ test('should display 2 different times', async ({ page }) => {
   await page.goto('/')
   await expect(page.locator('.time-display .time')).toHaveCount(2)
 })
+
+/* Parmeterized test */
+;[
+  { language: 'EN', alphabeticalTime: '05:39:47' },
+  { language: 'NL', alphabeticalTime: '02:37:27' },
+  { language: 'ES', alphabeticalTime: '11:40:29' },
+  { language: 'DE', alphabeticalTime: '02:46:59' },
+  { language: 'FR', alphabeticalTime: '11:43:16' },
+].forEach(({ language, alphabeticalTime }) => {
+  test(`should display ${alphabeticalTime} for language ${language}`, async ({ page }) => {
+    await page.goto('/')
+    await page.locator(`.language-selector .lang:has-text('${language}')`).click()
+    await expect(page.locator('.time-display .time-display-time')).toHaveText([
+      '13:34:12',
+      alphabeticalTime,
+    ])
+  })
+})
