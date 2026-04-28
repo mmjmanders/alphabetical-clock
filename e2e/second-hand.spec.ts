cede7e48945406382926383b7ad659bdb8a19ff5
Mark Manders <mmjmanders@gmail.com>
2026-04-28 09:16:42 +0200

    Extract shared clock fixture for e2e tests and update specs to use it

--- e2e/second-hand.spec.ts
+++ e2e/second-hand.spec.ts
@@ -1,4 +1,5 @@
-import { expect, test } from '@playwright/test'
+import { expect } from '@playwright/test'
+import { test } from './clock-fixture'
 
 test('should have second hand visible', async ({ page }) => {
   await page.goto('/')
