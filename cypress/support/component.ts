c7791ff5f8d20a5c0ffa8f7329a21639e7f525df
Mark Manders <mmjmanders@gmail.com>
2026-04-30 21:08:58 +0200

    Merge and refactor E2E tests for title and version into app spec; update Cypress setup with exception handling and intercept for Iconify requests

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -9,3 +9,23 @@ declare global {
 }
 
 Cypress.Commands.add('mount', mount)
+
+// @iconify/vue v5 accesses instance.appContext.app in a watcher that can fire
+// after Cypress unmounts the component, at which point appContext is already undefined.
+Cypress.on('uncaught:exception', (err) => {
+  if (err.message.includes("Cannot read properties of undefined (reading 'app')")) {
+    return false
+  }
+})
+
+beforeEach(() => {
+  cy.intercept(
+    {
+      method: 'GET',
+      url: 'https://api.iconify.design/*',
+    },
+    {
+      statusCode: 200,
+    },
+  )
+})
